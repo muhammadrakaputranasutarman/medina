c64242d4aab9f229589093e31feaa70c828e15ba
pajarr <pajarm2326@gmail.com>
2022-11-07 11:29:26 +0700

    delete import cli

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
-
 
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
