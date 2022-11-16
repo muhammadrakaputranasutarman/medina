414fc846e66bd0930294f2ff3971630bb82bfc81
pajarr <pajarm2326@gmail.com>
2022-11-16 13:46:05 +0700

    update navbar

--- src/_nav.js
+++ src/_nav.js
@@ -1,26 +1,78 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 import {
-  cilBell,
-  cilCalculator,
-  cilChartPie,
-  cilCursor,
-  cilDescription,
-  cilDrop,
-  cilNotes,
-  cilPencil,
+  // cilBell,
+  // cilCalculator,
+  // cilChartPie,
+  // cilCursor,
+  // cilDescription,
+  // cilDrop,
+  // cilNotes,
+  // cilPencil,
   cilPuzzle,
-  cilSpeedometer,
-  cilStar,
+  // cilSpeedometer,
+  // cilStar,
 } from '@coreui/icons'
-import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
-
+import { CNavGroup, CNavItem } from '@coreui/react'
+// CNavTitle
 const _nav = [
   {
-    component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+    component: CNavGroup,
+    name: 'Setup CRUD',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Users',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Menu',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Role Akses',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Company Profile',
+        to: '/base/carousels',
+      },
+      {
+        component: CNavItem,
+        name: 'Partner Kami',
+        to: '/base/collapses',
+      },
+      {
+        component: CNavItem,
+        name: 'Struktur Organisasi',
+        to: '/base/list-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Produk',
+        to: '/base/navs',
+      },
+      {
+        component: CNavItem,
+        name: 'Detail Produk',
+        to: '/base/paginations',
+      },
+      {
+        component: CNavItem,
+        name: 'Berita',
+        to: '/base/placeholders',
+      },
+      {
+        component: CNavItem,
+        name: 'Kategori berita',
+        to: '/base/popovers',
+      },
+    ],
   },
   // {
   //   component: CNavTitle,
@@ -42,84 +94,7 @@ const _nav = [
   //   component: CNavTitle,
   //   name: 'Components',
   // },
-  // {
-  //   component: CNavGroup,
-  //   name: 'Base',
-  //   to: '/base',
-  //   icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-  //   items: [
-  //     {
-  //       component: CNavItem,
-  //       name: 'Accordion',
-  //       to: '/base/accordion',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Breadcrumb',
-  //       to: '/base/breadcrumbs',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Cards',
-  //       to: '/base/cards',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Carousel',
-  //       to: '/base/carousels',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Collapse',
-  //       to: '/base/collapses',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'List group',
-  //       to: '/base/list-groups',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Navs & Tabs',
-  //       to: '/base/navs',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Pagination',
-  //       to: '/base/paginations',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Placeholders',
-  //       to: '/base/placeholders',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Popovers',
-  //       to: '/base/popovers',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Progress',
-  //       to: '/base/progress',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Spinners',
-  //       to: '/base/spinners',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Tables',
-  //       to: '/base/tables',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Tooltips',
-  //       to: '/base/tooltips',
-  //     },
-  //   ],
-  // },
+
   // {
   //   component: CNavGroup,
   //   name: 'Buttons',
